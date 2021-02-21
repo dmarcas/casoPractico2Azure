812c26bcddc2b2d3b488cde20bdada2033847b15
David Marcas <david.marcas@gmail.com>
2021-02-22 00:11:38 +0100

    added vm creation with terraform

---
+++ terraform/vm4-network.tf
@@ -0,0 +1,37 @@
+# Create NIC
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface
+
+resource "azurerm_network_interface" "myNic4" {
+  name                = "vmnic4"  
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+    name                           = "myipconfiguration1"
+    subnet_id                      = azurerm_subnet.mySubnet.id 
+    private_ip_address_allocation  = "Static"
+    private_ip_address             = "10.0.1.13"
+    public_ip_address_id           = azurerm_public_ip.myPublicIp4.id
+  }
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+# IP pública
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip
+
+resource "azurerm_public_ip" "myPublicIp4" {
+  name                = "vmip4"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
